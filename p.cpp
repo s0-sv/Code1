cce1e7d5327038778d7f38148284676bd2f70131
Zakharova Sofia <sofia_zakharova@knu.ua>
2025-09-19 22:04:51 +0300

    Add splitDeck() function

--- p.cpp
+++ p.cpp
@@ -13,10 +13,6 @@ public:
 };
 
 class Deck {
-    /*int suits;
-    int ranks; // за замовчуванням 13
-    vector<Card> cards;
-    int pos; */
 public:
     int suits;
     int ranks; // за замовчуванням 13
@@ -56,8 +52,7 @@ void readInput(int &suits, int &n) {
             cout << "Треба ввести натуральне число. Спробуйте ще раз.\n";
         }
     }
-    //    cout << "Введіть кількість карт в масті: ";
-    //    cin >> ranks;
+
     while (true) {
         cout << "Введіть кількість карт для роздачі: ";
         if (cin >> n) break;
@@ -69,11 +64,41 @@ void readInput(int &suits, int &n) {
     }
 }
 
+void splitDeck(vector<int> &deckparts, Deck &deck, int n) {
+    int currenlen = 0;
+    Card prev = deck(); // перша карта
+    cout << prev.rank << " ";
+
+    currenlen = 1;
+    for(int i = 1; i < n; i++) {
+        Card c = deck(); //витягуємо наступну карту
+        if(c >= prev) { //"Поки наступна карта старша за попередню або тієї самої вартості, вона кладеться в стопку"
+            currenlen++;
+        } else {
+            deckparts.push_back(currenlen); //зберігли довжину стопки
+            currenlen = 1;
+        }
+        prev = c;
+        cout << prev.rank << " ";
+    }
+    deckparts.push_back(currenlen);
+    cout << "\n";
+    cout << "Довжини стопок: " << "\n";
+    for (int len: deckparts ) {
+        cout << len << " ";
+        }
+    cout << "\n";
+}
+
 int main() {
 try {
     int suits, n;
     readInput(suits, n);
     Deck deck(suits, 13); //створили нашу колоду
+
+    vector<int> deckparts; //довжини стопок в різних колодах
+    splitDeck(deckparts, deck, n);
+
     } catch (const exception &e) {
         cerr << "Сталася помилка: " << e.what() << "\n";
     } catch (...) {
